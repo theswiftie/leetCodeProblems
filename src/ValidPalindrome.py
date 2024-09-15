e509901e3c72be4e41418337b62a618aec10517d
Nandika K <2nandika.k@gmail.com>
2024-09-14 23:11:32 -0400

    Revised for faster runtime

--- src/ValidPalindrome.py
+++ src/ValidPalindrome.py
@@ -1,16 +1,12 @@
 class Solution (object):
     def isPalindrome(self, s):
-        if len(s) == 0 or len(s) == 0:
-            return True
-        elif (s[0] == s[-1]):
-            return self.isPalindrome(s[1:-1])
-        else:
-            return False;
+        phrase = ""
+        for letter in s:
+            if letter.isalnum():
+                phrase += letter.lower()
+
+        return phrase == phrase[::-1]
 
 sol = Solution()
-s = "stats"
-print(sol.isPalindrome(s));
-s = "lol"
-print(sol.isPalindrome(s));
-s = "naan"
-print(sol.isPalindrome(s));
+print(sol.isPalindrome("race a car"));
+print(sol.isPalindrome("A man, a plan, a canal: Panama"))
