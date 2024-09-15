d5a4d4525e2437c5656e04f5380446637534a016
Nandika K <2nandika.k@gmail.com>
2024-09-15 01:33:04 -0400

    FizzBuzz memory and time complexity improvements

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -1,16 +1,30 @@
 class Solution(object):
     def fizzBuzz(self, n):
-        fizz_indices = range(2, n, 3)
-        buzz_indices = range(4, n, 5)
-        fizzbuzz_indices = range(14, n, 15)
+        # fizz_indices = range(2, n, 3)
+        # buzz_indices = range(4, n, 5)
+        # fizzbuzz_indices = range(14, n, 15)
         
-        arr = [str(i) for i in range(1, n + 1)]
+        # arr = [str(i) for i in range(1, n + 1)]
         
-        if n >= 3: arr[2:n+1:3] = ["Fizz"] * len(fizz_indices)
-        if n >= 5: arr[4:n+1:5] = ["Buzz"] * len(buzz_indices)
-        if n >= 15: arr[14:n+1:15] = ["FizzBuzz"] * len(fizzbuzz_indices)
+        # if n >= 3: arr[2:n+1:3] = ["Fizz"] * len(fizz_indices)
+        # if n >= 5: arr[4:n+1:5] = ["Buzz"] * len(buzz_indices)
+        # if n >= 15: arr[14:n+1:15] = ["FizzBuzz"] * len(fizzbuzz_indices)
 
+        # return arr
+
+        arr = []
+
+        for i in range(1,n+1):
+            if i % 3 == 0:
+                if i % 5 == 0:
+                    arr.append("FizzBuzz")
+                else: arr.append("Fizz")
+            elif i % 5 == 0:
+                arr.append("Buzz")
+            else:
+                arr.append(str(i))
         return arr
          
 sol = Solution()
 print(sol.fizzBuzz(15))
+print(sol.fizzBuzz(45))
