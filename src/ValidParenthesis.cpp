de801590164d150abd4ff3f0097550880ac7ac18
Nandika K <2nandika.k@gmail.com>
2024-09-21 11:55:26 -0400

    Valid Parenthesis problem

---
+++ src/ValidParenthesis.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> parenStack;
+
+        for (char brace: s) {
+            if (brace == '{' || brace == '[' || brace == '(') {
+                parenStack.push(brace);
+            }
+            else if (!parenStack.empty() && (parenStack.top() == '{' && brace == '}' || parenStack.top() == '[' && brace == ']' || parenStack.top() == '(' && brace == ')')){
+                parenStack.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main(int argc, char const *argv[]){
+    Solution sol = Solution();
+    cout << sol.isValid("({})");
+    return 0;
+}
