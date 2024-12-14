878675716666293be94da8b7a8e1d265d360c0c8
Nandika K <2nandika.k@gmail.com>
2024-12-14 11:19:52 -0500

    working on atoi

---
+++ src/atoi.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int myAtoi(string s) {
+    int num = 0;
+    int neg = 1;
+
+    // consume whitespace
+    while(s[0] == ' ') {
+        s = s.erase(0,1);
+    }
+    // check for sign
+    if (s[0] == '-' || s[0] == '+') {
+        if (s[0] == '-') neg = -1;
+        s = s.erase(0,1);
+    }
+
+    while (isdigit(s[0])) {
+        
+    }
+    return num;
+}
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
