1c66f5c7593c3c8c7ae27a0dba759af99d842d77
Nandika K <2nandika.k@gmail.com>
2024-12-14 11:20:10 -0500

    ig i worked on houserobber at some point

--- src/HouseRobber.cpp
+++ src/HouseRobber.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        
+        for (int i = 0; i < nums.size(); i += 2) {
+            
+        }
+
     }
 };
 
 int main (int argc, const char* argv) {
-    
+    vector<int> houses = {2,7,9,3}
+    cout << (rob()) << endl;
 }
