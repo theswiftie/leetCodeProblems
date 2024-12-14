f57648bd90c301c0a9d6d481264125b74b597d68
Nandika K <2nandika.k@gmail.com>
2024-12-14 11:21:43 -0500

    think this works (pretesting)

--- src/atoi.cpp
+++ src/atoi.cpp
@@ -15,8 +15,15 @@ int myAtoi(string s) {
         s = s.erase(0,1);
     }
 
+    // consumer digits
     while (isdigit(s[0])) {
-        
+        if (num != 0) {
+            num = num * 10 + s[0];
+        }
+        else {
+            num = s[0];
+        }
+        s = s.erase(0,1);
     }
     return num;
 }
