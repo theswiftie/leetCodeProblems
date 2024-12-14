3532cecca564c89f7f6b48218de2451529b064dc
Nandika K <2nandika.k@gmail.com>
2024-12-14 11:35:12 -0500

    fixed sizing issue

--- src/atoi.cpp
+++ src/atoi.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int myAtoi(string s) {
     int num = 0;
     int sign = 1;
+    int MIN = std::numeric_limits<int>::min();
+    int MAX = std::numeric_limits<int>::max();
 
     // consume whitespace
     while(s[0] == ' ') {
@@ -17,13 +19,21 @@ int myAtoi(string s) {
 
     // consumer digits
     while (isdigit(s[0])) {
-            if (num != 0) {
+        if (num * 10 + (s[0] - '0') > MAX) {
+            num = MAX;
+            break;
+        }
+        else if (num * 10 + (s[0] - '0') < MIN) {
+            num = MIN;
+            break;
+        }
+        else if (num != 0) {
                 num = num * 10 + (s[0] - '0');
-            }
-            else {
-                num = s[0] - '0';
-            }
-            s = s.erase(0,1);
+        }
+        else {
+            num = s[0] - '0';
+        }
+        s = s.erase(0,1);
     }    
     num *= sign;
     return num;
